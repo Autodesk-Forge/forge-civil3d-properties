bc8854b345e8463c3f11edc905da2b4f14f35267
Augusto Goncalves <augusto.goncalves@autodesk.com>
2020-06-22 16:22:37 -0700

    first commit

---
+++ extractStyles/Commands.cs
@@ -0,0 +1,97 @@
+﻿using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.ApplicationServices.Core;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.Runtime;
+using Autodesk.Civil.DatabaseServices;
+using Autodesk.Civil.DatabaseServices.Styles;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.IO;
+using System.Reflection;
+using System.Text.RegularExpressions;
+
+namespace extractStyles
+{
+  public class Commands
+  {
+    [CommandMethod("extractStyles")]
+    public static void ExtracStyles()
+    {
+      // this should contain all Tin Srufaces with respective array of points
+      JArray styleInformation = new JArray();
+
+      Document doc = Application.DocumentManager.MdiActiveDocument;
+      Database db = doc.Database;
+      using (Transaction trans = doc.TransactionManager.StartTransaction())
+      {
+        BlockTable bt = trans.GetObject(db.BlockTableId, OpenMode.ForRead) as BlockTable;
+        BlockTableRecord mSpace = trans.GetObject(bt[BlockTableRecord.ModelSpace], OpenMode.ForRead) as BlockTableRecord;
+
+        foreach (ObjectId entId in mSpace)
+        {
+          // try open as Civil 3D entity, otherwise skip
+          Autodesk.Aec.DatabaseServices.Entity aecEnt = trans.GetObject(entId, OpenMode.ForRead) as Autodesk.Aec.DatabaseServices.Entity;
+          if (aecEnt == null) continue;
+
+          // Handle is exposed as "externalId" in the Viewer
+          dynamic entityInfo = new JObject();
+          entityInfo.handle = aecEnt.Handle.ToString();
+          entityInfo.style = new JArray();
+
+          StyleBase style;
+          try { style = trans.GetObject(aecEnt.StyleId, OpenMode.ForRead) as StyleBase; }
+          catch { continue; }
+
+          dynamic nameProp = new JObject();
+          nameProp.name = "Name";
+          nameProp.value = style.Name;
+          entityInfo.style.Add(nameProp);
+
+          Type styleType = style.GetType();
+          foreach (PropertyInfo prop in styleType.GetProperties())
+          {
+            try
+            {
+              // if the property is not a Civil 3D property, skip...
+              if (!prop.DeclaringType.FullName.Contains("Civil")) continue;
+
+              string propName = prop.Name;
+              object rawValue = prop.GetValue(style);
+              string formattedValue = string.Empty;
+
+              if (rawValue is String)
+                formattedValue = rawValue as string;
+              else if (rawValue is ObjectId)
+              {
+                StyleBase subStyle = trans.GetObject((ObjectId)rawValue, OpenMode.ForRead) as StyleBase;
+                if (subStyle == null) return;
+
+                formattedValue = subStyle.Name;
+              }
+
+              if (string.IsNullOrWhiteSpace(formattedValue)) continue;
+
+              dynamic styleProp = new JObject();
+              styleProp.name = Regex.Replace(propName, "(\\B[A-Z])", " $1");
+              styleProp.value = formattedValue;
+              entityInfo.style.Add(styleProp);
+            }
+            catch { }
+          }
+
+          styleInformation.Add(entityInfo);
+        }
+
+        trans.Commit();
+      }
+
+      // save all to a .json file
+      using (StreamWriter file = File.CreateText("result.json"))
+      using (JsonTextWriter writer = new JsonTextWriter(file))
+      {
+        styleInformation.WriteTo(writer);
+      }
+    }
+  }
+}
---
+++ webapp/wwwroot/js/C3DCustomProps.js
@@ -0,0 +1,120 @@
+// *******************************************
+// Custom Property Panel
+// *******************************************
+class CustomPropertyPanel extends Autodesk.Viewing.Extensions.ViewerPropertyPanel {
+  constructor(viewer, options) {
+    super(viewer, options);
+    this._data = null;
+  }
+  setProperties(properties, options) {
+    Autodesk.Viewing.Extensions.ViewerPropertyPanel.prototype.setProperties.call(this, properties, options);
+
+    if (this._data != null) {
+      this.viewer.getProperties(this.propertyNodeId, (props) => {
+        const handle = props.externalId;
+
+        this._data.forEach((d) => {
+          if (d.handle !== handle) return;
+
+          d.style.forEach((s) => {
+            this.addProperty(s.name, s.value, 'Style');
+          })
+        })
+      })
+    }
+  }
+
+  setNodeProperties(nodeId) {
+    Autodesk.Viewing.Extensions.ViewerPropertyPanel.prototype.setNodeProperties.call(this, nodeId);
+    this.nodeId = nodeId; // store the dbId for later use
+  }
+}
+
+// *******************************************
+// Custom Property Panel Extension
+// *******************************************
+class CustomPropertyPanelExtension extends Autodesk.Viewing.Extension {
+  constructor(viewer, options) {
+    super(viewer, options);
+    //window._connection = null;
+    //window._connectionId = null;
+    this._panel = null;
+    this._data = null;
+  }
+
+  load() {
+    this.startConnection(() => {
+      var params = this.options.itemId.split('/');
+      this.options.projectId = params[params.length - 3];
+
+      $.notify("Requesting style information... please wait.", "info");
+      jQuery.post({
+        url: '/api/styles',
+        contentType: 'application/json',
+        data: JSON.stringify({ 'connectionId': window._connectionId, 'activity': 'extractStyles', 'itemId': this.options.itemId, 'versionId': this.options.versionId }),
+        success: function (res) {
+          $.notify("Design Automation Workitem started... please wait.", "info");
+        },
+        error: function (err) {
+          $.notify("Fail to trigger Design Automation to extract style information", "error");
+        }
+      });
+    });
+
+    return true;
+  }
+
+  unload() {
+    if (this._panel == null) return;
+    var ext = this.viewer.getExtension('Autodesk.PropertiesManager');
+    this._panel = null;
+    ext.setDefaultPanel();
+    return true;
+  }
+
+  onToolbarCreated() {
+    this._panel = new CustomPropertyPanel(this.viewer, this.options);
+    this.viewer.addEventListener(Autodesk.Viewing.EXTENSION_LOADED_EVENT, (e) => {
+      if (e.extensionId !== 'Autodesk.PropertiesManager') return;
+      var ext = this.viewer.getExtension('Autodesk.PropertiesManager');
+      ext.setPanel(this._panel);
+    })
+  }
+
+  startConnection(onReady) {
+    if (window._connection != undefined && window._connection.connectionState) { this.defineHandles(); if (onReady) onReady(); return; }
+    window._connection = new signalR.HubConnectionBuilder().withUrl("/api/signalr/designautomation").build();
+    window._connection.start()
+      .then(() => {
+        window._connection.invoke('getConnectionId')
+          .then((id) => {
+            window._connectionId = id; // we'll need this...
+            if (onReady) onReady();
+          });
+      });
+  }
+
+  defineHandles() {
+    window._connection.on("propsReady", (url) => {
+      jQuery.get({
+        url: url,
+        success: (res) => {
+          this._panel._data = JSON.parse(res);
+          $.notify("Style information ready to use.", "success");
+        },
+        error: (err) => {
+
+        }
+      });
+    });
+
+    window._connection.on("onCompleteProps", (workItemStatus) => {
+      console.log(workItemStatus);
+      workItemStatus = JSON.parse(workItemStatus);
+      if (workItemStatus.status !== 'success')
+        $.notify("Fail to extract style information", "error");
+    });
+  }
+}
+
+Autodesk.Viewing.theExtensionManager.registerExtension('Autodesk.Sample.CustomPropertyPanelExtension', CustomPropertyPanelExtension);
